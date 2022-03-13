4043150e502d6e37a9078f66d0ee685be374c4d3
Pingu9191 <99191235+Pingu9191@users.noreply.github.com>
2022-03-13 20:28:19 +0100

    Update command.c

--- command.c
+++ command.c
@@ -1,10 +1,11 @@
 /** 
- * @brief It implements the command interpreter
+ * @brief It implements the command interpreter to read, control and define all the commands in the ant game
  * 
  * @file command.c
  * @author Profesores PPROG
- * @version 2.1
- * @date 19-02-2022 
+ * Modified by Ignacio Nunnez && Nicolas Victorino 
+ * @version 3.0
+ * @date 11-03-2022 
  * @copyright GNU Public License
  */
 
@@ -26,7 +27,8 @@ char *cmd_to_str[N_CMD]
       {"r", "Right"},
       {"l", "Left"},
       {"t", "Take"},
-      {"d", "Drop"}};
+      {"d", "Drop"},
+      {"a", "Attack"}};
 
 /*
 * Gets the last command that the user inputted and assigns them a numeric value depending of what they do
@@ -35,14 +37,17 @@ T_Command command_get_user_input(char *arg)
 {
   /*Assigns a value to the newly created variable "cmd", that corresponds to the command: UNKNOWN (state by default of a command)*/
   T_Command cmd = NO_CMD;
-  char *input;
+  char *input; //Para probar modifiacion cambiar buffer por input(nombre)
   char *second;
   char buffer[CMD_LENGHT] = "";
   int i = UNKNOWN - NO_CMD + 1;
 
   /*Compares the command inputted to all the commands available in the game, and when it finds a match, it outputs the numeric value of that command, assigning it to the variable "cmd"*/
   
-  scanf("%s", buffer);
+  //scanf("%s", input);
+
+  
+  fgets(buffer, CMD_LENGHT, stdin);
   input = strtok(buffer, " ");
   second = strtok(NULL, " ");
   if (second != NULL) {
@@ -50,6 +55,7 @@ T_Command command_get_user_input(char *arg)
   } else {
     arg[0] = 0;
   }
+  
 
     cmd = UNKNOWN;
     while (cmd == UNKNOWN && i < N_CMD)
